102a932f6d9a968ec2bfcf37c4557256a29265f2
Fabrizio302002 <jorgefabrizioolanofarfan26@gmail.com>
2024-09-11 22:12:12 -0500

    Primer Commmit

---
+++ src/main/java/com/prestamo/DawiIiSemana01Application.java
@@ -0,0 +1,13 @@
+package com.prestamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiIiSemana01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiIiSemana01Application.class, args);
+	}
+
+}
